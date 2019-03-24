3c93d3857c55b76987484299b4ec760e1337aa9a
greenmon <greenmon@kaist.ac.kr>
2019-03-24 23:47:54 +0900

    Minor changes in request interface

--- python/raw-image-processing/mock_request.py
+++ python/raw-image-processing/mock_request.py
@@ -4,6 +4,7 @@ import time
   
 # defining the api-endpoint  
 API_ENDPOINT = "http://localhost:8080/api/image"
+# API_ENDPOINT = "http://52.78.16.193:8080/api/image"
 
 n = 16
 
@@ -11,8 +12,8 @@ for i in range(n*n):
     cell = open('./cells/cell' + str(i), 'r').read()
 
     # data to be sent to api 
-    data = {'number': i, 
-            'cell': cell} 
+    data = {'n': i, 
+            'c': cell} 
   
     # sending post request and saving response as response object 
     r = requests.post(url = API_ENDPOINT, data = data)
--- python/raw-image-processing/slice_image.py
+++ python/raw-image-processing/slice_image.py
@@ -4,7 +4,7 @@ from PIL import Image
 arr = open("output", "rb").read() 
 
 np_arr = np.frombuffer(arr, dtype=np.uint8)
-np_arr = np.reshape(np_arr, (160, 160))
+np_arr = np.reshape(np_arr, (-1, 160))
 
 grid = 16
 size = int(160/grid)
@@ -14,8 +14,10 @@ def bytesToHexString(bytes):
 
 for i in range(grid):
     for j in range(grid):
-        cell = np_arr[size*j:size*(j+1), size*i:size*(i+1)]
-        cell = np.reshape(cell, (size*size))
+                    
+        cell = np_arr[size*i:size*(i+1), size*j:size*(j+1)]
+
+        cell = cell.flatten()
 
         hex = bytesToHexString(cell)
         
