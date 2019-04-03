b7305b8fdf4510363751eb337f1c6f562a49948e
greenmon <greenmon@kaist.ac.kr>
2019-04-03 14:36:20 +0900

    Improve mural test for large input
    Imporve mural test for large input, where N > 10^5
    
    Continuous sum can be calculated as transformation of previous sum.

--- python/kick_start_google/mural_test.py
+++ python/kick_start_google/mural_test.py
@@ -2,23 +2,34 @@ import pytest
 
 # Compute maximum total beauty score
 
-def max_total_score(scores):
-  length = (len(scores) + 1) // 2
-  max_sum = 0
+def max_total_score(scores, total_length):
+  length = (total_length + 1) // 2
 
-  for i in range(len(scores) - length + 1):
-    subsequence = scores[i:i+length]
-    current_sum = sum(subsequence)
+  max_sum = prev_sum = sum_of_digits_in_subsequence(scores, 0, length)
+
+  for i in range(1, total_length - length + 1):
+    current_sum = prev_sum - int(scores[i - 1]) + int(scores[i + length - 1])
+    prev_sum = current_sum
     if (current_sum > max_sum):
       max_sum = current_sum
 
   return max_sum
 
+def sum_of_digits_in_subsequence(scores, start, length):
+  sum = 0;
+  for i in range(start, start + length):
+    sum += int(scores[i])
+  return sum;
+
+def test_sum_of_digits():
+  assert 10 == sum_of_digits_in_subsequence('1234', 0, 4)
 
 def test_max_total_score():
-  scores = [1, 3, 3, 2]
+  s1 = '1332'
+  s2 = '4321'
   
-  assert 6 == max_total_score(scores)
+  assert 6 == max_total_score(s1, 4)
+  assert 7 == max_total_score(s2, 4)
 
 
 if __name__ == "__main__":
@@ -26,7 +37,6 @@ if __name__ == "__main__":
 
   for i in range(T):
     N = int(input())
-    numbers = list(input())
-    scores = [int(number) for number in numbers]
+    scores = input()
 
-    print('Case #{n}: {score}'.format(n=i+1, score=max_total_score(scores)))
+    print('Case #{n}: {score}'.format(n=i+1, score=max_total_score(scores, N)))
