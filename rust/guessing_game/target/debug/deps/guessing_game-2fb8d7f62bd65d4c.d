a44ad26acbf9ddbcc0c4e8d2aafb136957f10fe3
greenmon <greenmon@kaist.ac.kr>
2019-10-04 16:49:09 +0900

    Add rust basic tutorials

---
+++ rust/guessing_game/target/debug/deps/guessing_game-2fb8d7f62bd65d4c.d
@@ -0,0 +1,5 @@
+/Users/greenmon/daily-coding/rust/guessing_game/target/debug/deps/guessing_game-2fb8d7f62bd65d4c: src/main.rs
+
+/Users/greenmon/daily-coding/rust/guessing_game/target/debug/deps/guessing_game-2fb8d7f62bd65d4c.d: src/main.rs
+
+src/main.rs:
---
+++ rust/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/greenmon/daily-coding/rust/guessing_game/target/debug/guessing_game.dSYM: /Users/greenmon/daily-coding/rust/guessing_game/src/main.rs
